e71abb22a2c63e2161651d62096c12a0355dcdd1
davidegaspar <me@davidegaspar.com>
2015-10-08 02:23:51 +0100

    setup slider and controls

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 window.addEventListener('load',function(){
   //ui
   var
+  jobs = document.querySelector('.jobs'),
   slider = document.querySelector('.jobs .slider-box .slider'),
   slideTpl = new Template(slider),
   leftControl = document.querySelector('.jobs .slider-box .left.control'),
@@ -8,23 +9,42 @@ window.addEventListener('load',function(){
   //get jobs
   get=new Gate('http://localhost:3000/jobs');
   get.ok(function(data){
+    var rawData=ext(data);
+    //template
+    slideTpl.gen(rawData);
+    // check
+    if (rawData.length<2) return;
     //build slider
-    // var s = new Slider(slider,
-    //   {
-    //     sInterval:3000,
-    //     tIn:'slide_fade',
-    //     tOut:'slide_fade',
-    //     tDuration:'1000ms',
-    //     tFunction:'linear',
-    //     tCross:0,
-    //     iFirst:true
-    //   },
-    //   null,//willChange
-    //   null//didChange
-    // );
-    // s.play();
-    //console.log();
-    slideTpl.gen(ext(data));
+    var s = new Slider(slider,
+      {
+        sInterval:3000,
+        tIn:'slide_fade',
+        tOut:'slide_fade',
+        tDuration:'250ms',
+        tFunction:'linear',
+        tCross:0,
+        iFirst:true
+      },
+      null,//willChange
+      null//didChange
+    );
+    // controls
+    leftControl.classList.add('show');
+    leftControl.addEventListener('click',function(){
+      s.wait();s.prev();
+    });
+    rightControl.classList.add('show');
+    rightControl.addEventListener('click',function(){
+      s.wait();s.next();
+    });
+    jobs.addEventListener('mouseover',function(){
+      s.pause();
+    });
+    jobs.addEventListener('mouseout',function(){
+      s.play();
+    });
+    //play
+    s.play();
   });
   get.fail(function(err){
     //fallback
