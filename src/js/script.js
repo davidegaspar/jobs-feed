4c7b25cd1318c06308d96de0fde33c500239416f
davidegaspar <me@davidegaspar.com>
2015-10-08 20:32:33 +0100

    edge case no jobs or request error

--- src/js/script.js
+++ src/js/script.js
@@ -6,14 +6,23 @@ window.addEventListener('load',function(){
   slideTpl = new Template(slider),
   leftControl = document.querySelector('.jobs .slider-box .left.control'),
   rightControl = document.querySelector('.jobs .slider-box .right.control'),
+  error = document.querySelector('.jobs .slider-box .error');
   //get jobs
-  get=new Gate('http://localhost:3000/onejob');
+  var get=new Gate('http://localhost:3000/nojobs');
   get.ok(function(data){
     var rawData=ext(data);
+    // if empty
+    if (rawData.length===0){
+      error.classList.add('show');
+      return;
+    }
     //template
     slideTpl.gen(rawData);
-    // check
-    if (rawData.length<2) return;
+    slider.classList.add('show');
+    // if only one
+    if (rawData.length==1) {
+      return;
+    }
     //build slider
     var s = new Slider(slider,
       {
@@ -48,6 +57,7 @@ window.addEventListener('load',function(){
   });
   get.fail(function(err){
     //fallback
+    error.classList.add('show');
     console.log(err);
   });
   get.send();
